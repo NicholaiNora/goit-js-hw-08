1e7f92eb260c62a2de40abac7844c7da19f6a191
NicholaiNora <nicholai.nora.dict@gmail.com>
2024-04-21 17:23:36 +0800

    Homework Module 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -13,4 +13,19 @@ player.on(
 );
 const JCurrentTime = localStorage.getItem('videoplayer-current-time') ?? "";
 const currentTime = JSON.parse(JCurrentTime);
-player.setCurrentTime(currentTime.seconds);
+player
+  .setCurrentTime(currentTime.seconds)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
